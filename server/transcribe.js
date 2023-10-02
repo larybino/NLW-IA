730c5412303e0949dbed5892743226f7d0d234ed
larybino <binolaryssa@gmail.com>
2023-10-02 00:18:38 -0300

    first commit

---
+++ server/transcribe.js
@@ -0,0 +1,31 @@
+import {pipeline} from '@xenova/transformers'
+
+import { transcriptionExample } from "./utils/transcription.js"
+export async function transcribe(audio){
+   // return transcriptionExample
+
+
+   try {
+    
+
+    console.log("Realizando a transcrição do vídeo")
+    const transcribe = await pipeline(
+        "automatic-speech-recognition", 
+        "Xenova/whisper-small"
+    )
+
+
+    const transcription = await transcribe(audio, {
+        chunk_lenght_s: 30,
+        stride_lenght_s: 5,
+        language: "portuguese",
+        task: "transcribe",
+    })
+
+
+    console.log("Transcrição realizada")
+    return transcription?.text.replace("[Música]", "")
+   } catch (error) {
+    throw new Error(error)
+   }
+}
